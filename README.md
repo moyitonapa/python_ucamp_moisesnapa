c74cb32eea6c347806595d67ad6a262753190a98
moyitonapa <moyitonp@gmail.com>
2025-08-11 20:11:55 -0600

    ok

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Al solicitar la altura se destaco con el formato de entrada de tipo flotante.
 El IMC se calcula dividiendo la masa por la altura elevada al cuadrado.
 
 Al tener la informacion necesaria se calcula el IMC y se imprime el resultad donde se uso la etiqueta round para que el resultado del IMC sea redondeado a 2 decimales. 
+
+Aunado a lo anterior se agrego un while True para que el programa no se quede en un bucle infinito si el usuario ingresa un numero no valido, ademas de de usar la etiqueta try y except para controlar si la entrada es valido o no.
--- imc_moisesnapa.py
+++ imc_moisesnapa.py
@@ -4,9 +4,25 @@
 print('¡Conoce tú estado de nutrición!')  
 print('Vámos a calcular tu IMC\n')
 nombre = input('Para iniciar es necesario que nos proporiones tu nombre completo: ') # le pedimos el nombre
-edad = int(input('¿Cuántos años tienes?')) # le pedimos la edad
-peso = float(input('¿Cuánto pesas en kilogramos?')) # le pedimos la masa
-altura = float(input('¿Cuánto mides en metros?\n')) # le pedimos la altura
-print(f'Nombre: {nombre}\nEdad: {edad}\nPeso: {peso}\nAltura: {altura}') # imprimimos la informacion
+while True:     
+            try: 
+                edad = int(input('¿Cuántos años tienes?')) # le pedimos la edad
+                break
+            except ValueError:
+                print('¡No es un numero entero! Por favor ingrese un numero entero')
+while True:
+            try:
+                peso = float(input('¿Cuánto pesas en kilogramos?')) # le pedimos la masa
+                break
+            except ValueError:
+                print('¡NO ES UN NUMERO FLOTANTE! Vuelve a intentarlo.')
+while True:
+            try:
+                altura = float(input('¿Cuánto mides en metros?')) # le pedimos la altura
+                break
+            except ValueError:
+                print('¡No es un numero flotante! Por favor ingrese un numero flotante')
+
+print(f'\nNombre: {nombre}\nEdad: {edad}\nPeso: {peso}\nAltura: {altura}') # imprimimos la informacion
 imc = peso / (altura ** 2) # calculamos el IMC
-print(f'{nombre} tú IMC es de: {round(imc, 2)}') # imprimimos el IMC
+print(f'\n{nombre} tú IMC es de: {round(imc, 2)}') # imprimimos el IMC
