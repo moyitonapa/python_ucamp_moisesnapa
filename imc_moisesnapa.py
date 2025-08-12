3da9b768e679e4a38a99235f4be20c7f08dd14bb
moyitonapa <moyitonp@gmail.com>
2025-08-11 20:38:30 -0600

    Update imc_moisesnapa.py

--- imc_moisesnapa.py
+++ imc_moisesnapa.py
@@ -1,4 +1,4 @@
-#Código que calcula el Indice de Maza Corporal (IMC)
+#Código que calcula el Índice de Maza Corporal (IMC)
 #---------Moisés Nápoles Pacheco---------#
 
 print('¡Conoce tú estado de nutrición!')  
@@ -26,3 +26,16 @@ while True:
 print(f'\nNombre: {nombre}\nEdad: {edad}\nPeso: {peso}\nAltura: {altura}') # imprimimos la informacion
 imc = peso / (altura ** 2) # calculamos el IMC
 print(f'\n{nombre} tú IMC es de: {round(imc, 2)}') # imprimimos el IMC
+
+if imc<15.00:
+    print('Tienes Delgadez severa')
+elif imc>=15.00 and imc<=16.99:
+            print('Tienes Delgadez moderada')
+elif imc>=17.00 and imc<=18.49:
+            print('Tienes Delgadez leve')
+elif imc>=18.50 and imc<=24.99:
+            print('Estas en tu peso ideal')
+elif imc>=25.00 and imc<=29.99:
+            print('Tienes Sobrepeso')
+else: 
+            print('Tienes Obesidad morbida')
