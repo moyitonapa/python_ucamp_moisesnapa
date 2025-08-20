876ff5a6544b5faca67dd8e1f2fb795fb99104bc
moyitonapa <moyitonp@gmail.com>
2025-08-20 12:38:28 -0600

    Create ejemplo.py

---
+++ ejemplo.py
@@ -0,0 +1,12 @@
+#ejemplos para entradas de usuarios 
+
+print("Vamos a comenzar con la practica para ingresar datos en una consola")
+
+nombre= input("Ingresa tu nombre porfavor: ")
+edad= input(int("Tu edad tambien: "))
+
+print("Hola", nombre, " nos dijiste que tienes ",edad, " años")
+if edad >= 18
+    print("tambien ya eres mayor de edad")
+else: 
+    print("No podemos condederte la entrada porque aun eres menor de edad")
