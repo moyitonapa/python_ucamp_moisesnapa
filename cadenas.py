1fb09796eb4f43f839f8c7ebc490f3fa092df56a
moyitonapa <moyitonp@gmail.com>
2025-09-02 18:37:32 -0600

    Create cadenas.py

---
+++ cadenas.py
@@ -0,0 +1,23 @@
+# texto_variado = 'Hola mundo'
+# print(type(texto_variado))
+
+# #se usan comillas triples para que el texto se muestre la cadena con formato
+# print("""
+# Funcionamiento de \
+# programa :(opciones)
+#       -1 Para acceder a opciones
+#       -2 Para salir
+# """)
+
+#Subscripting e indexado
+
+texto = 'Python'
+# print(texto[0])
+# print(texto[5])
+# print(texto[-1]) #al escribir numeros negativos solo vuelve a contar desde el ultimo elemnto 
+
+#no se pueden ingresar indices mayores al numero de caracteres de la cadena 
+#las cadenas de texto no se pueden cambiar de tipo de dato
+
+letra =texto[0]
+print(letra)
